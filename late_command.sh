0dd8e6b64444a8767d3c442121bce9b49b61ae3b
Max Goldman <maxg@meet.mit.edu>
2012-10-10 18:04:58 +0200

    More permissive firewall

--- late_command.sh
+++ late_command.sh
@@ -21,6 +21,9 @@ echo "key meet-lab-key {
 # DNS zones
 for db in /var/lib/bind/db.*.orig; do cp $db ${db/.orig/}; done
 
+# Firewall
+ufw allow from 192.168.30.0/24 to 192.168.30.1
+
 # sudo
 chmod 440 /etc/sudoers.d/*
 
